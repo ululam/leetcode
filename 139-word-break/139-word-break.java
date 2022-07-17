3e2a4b0e1af190f7ec1d6e27b8c29ce43889f266
Alexey Zakharchenko <alexey.zakharchenko@gmail.com>
2022-07-17 16:55:45 +0300

    Time: 16 ms (28.47%), Space: 46.9 MB (47.75%) - LeetHub

--- 139-word-break/139-word-break.java
+++ 139-word-break/139-word-break.java
@@ -2,40 +2,29 @@ class Solution {
     private int level = 0;
     
     public boolean wordBreak(String s, List<String> wordDict) {
-        // Gor through list, select all words that are in the string
-        // Add those words to set
-        // For each word:
-        // - (a) Remoev all occuremies. Remove word itself from set
-        // - If leftover len == 0 => true
-        // - If no, go to (a), with "contains" check.
-        // - If all words are interated, false
+        final Set<String> words = new HashSet<>(wordDict);
+        final Queue<Integer> queue = new LinkedList<>();
+        boolean[] visited = new boolean[s.length()];
+        queue.add(0);
         
-        Set<String> parts = new HashSet<>();
-        for (String w : wordDict) {
-            if (s.contains(w)) {
-                parts.add(w);
+        while (!queue.isEmpty()) {
+            int start = queue.poll();
+            if (visited[start]) {
+                continue;
             }
-        }
-        
-        return consists(s, parts, 0, new Boolean[s.length()]);
-    }
-    
-    private boolean consists(String s, Set<String> parts, int start, Boolean[] seen) {
-        if (start == s.length()) {
-            return true;
-        }
-        
-        if (seen[start] != null) {
-            return seen[start];
-        }
-        
-        for (int i=start+1;i<=s.length(); i++) {
-            String prefix = s.substring(start, i);
-            if (parts.contains(prefix) && consists(s, parts, i, seen)) {
-                return seen[start] = true;
+            
+            for (int i = start+1; i<=s.length(); i++) {
+                String sub = s.substring(start, i);
+                if (words.contains(sub)) {
+                    if (i == s.length()) {
+                        return true;
+                    }
+                    queue.add(i);
+                }
             }
+            visited[start] = true;
         }
         
-        return seen[start] = false;
+        return false;
     }
 }
