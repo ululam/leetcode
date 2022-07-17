2c8424cc2e2d31660ad614df1b4d747707724693
Alexey Zakharchenko <alexey.zakharchenko@gmail.com>
2022-07-17 19:09:27 +0300

    Time: 15 ms (33.27%), Space: 47.4 MB (27.72%) - LeetHub

--- 139-word-break/139-word-break.java
+++ 139-word-break/139-word-break.java
@@ -1,22 +1,23 @@
 public class Solution {
     public boolean wordBreak(String s, List<String> wordDict) {
         Set<String> words = new HashSet<>(wordDict);
-        boolean[] memo = new boolean[s.length() + 1];
+        boolean memo[] = new boolean[s.length() + 1];
         memo[0] = true;
         
-        for (int i=1; i<=s.length();i++) {
-            for (int j=0; j<i; j++) {
+        for (int i = 1; i <= s.length(); i++) {
+            for (int j = 0; j < i; j++) {
                 if (!memo[j]) {
                     continue;
                 }
-                String sub = s.substring(j,i);
-                if (words.contains(sub)) {
+                String secondPart = s.substring(j, i);
+                if (words.contains(secondPart)) {
                     memo[i] = true;
                     break;
                 }
             }
+            
         }
         
         return memo[s.length()];
-    }    
+    }
 }
