39d29dabcf023ec8ece2ad2b4e0ced31f298c2a5
Alexey Zakharchenko <alexey.zakharchenko@gmail.com>
2022-07-24 00:30:25 +0300

    Time: 217 ms (5.04%), Space: 74.7 MB (15.53%) - LeetHub

--- 542-01-matrix/542-01-matrix.java
+++ 542-01-matrix/542-01-matrix.java
@@ -14,9 +14,14 @@ class Solution {
             if (++curr0 > mat.length + mat[0].length) {
                 break;
             };
+            allSet = true;
             //System.out.printf("curr0 = %s\n", curr0);
             for (int i=0;i<mat.length;i++) {
                 for (int j=0;j<mat[i].length;j++) {
+                    if (mat[i][j] < Integer.MAX_VALUE) {
+                        continue;
+                    }
+                    allSet = false;
                     if (i > 0 && mat[i-1][j] == curr0) {
                         mat[i][j] = Math.min(curr0 + 1, mat[i][j]);
                         //System.out.printf("Set [%s][%s] = %s\n", i,j, mat[i][j]);
@@ -44,5 +49,12 @@ class Solution {
         return mat;
     }
     
-
+    private boolean setAndReturnIfChanged(int[][] mat, int i, int j, int val) {
+        if (mat[i][j] > val) {
+            mat[i][j] = val;
+            return true;
+        }
+        
+        return false;
+    }
 }
