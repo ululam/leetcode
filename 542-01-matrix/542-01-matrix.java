586a683f10e2982e4c820b8d161353054cf44a70
Alexey Zakharchenko <alexey.zakharchenko@gmail.com>
2022-07-24 00:20:55 +0300

    Time: 1699 ms (5.04%), Space: 46.3 MB (87.30%) - LeetHub

---
+++ 542-01-matrix/542-01-matrix.java
@@ -0,0 +1,48 @@
+class Solution {
+    public int[][] updateMatrix(int[][] mat) {
+        for (int i=0;i<mat.length;i++) {
+            for (int j=0;j<mat[i].length;j++) {
+                if (mat[i][j] == 1) {
+                    mat[i][j] = Integer.MAX_VALUE;
+                }
+            }
+        }
+
+        boolean allSet = false;
+        int curr0 = -1;
+        while (!allSet) {
+            if (++curr0 > mat.length + mat[0].length) {
+                break;
+            };
+            //System.out.printf("curr0 = %s\n", curr0);
+            for (int i=0;i<mat.length;i++) {
+                for (int j=0;j<mat[i].length;j++) {
+                    if (i > 0 && mat[i-1][j] == curr0) {
+                        mat[i][j] = Math.min(curr0 + 1, mat[i][j]);
+                        //System.out.printf("Set [%s][%s] = %s\n", i,j, mat[i][j]);
+                        continue;
+                    }
+                    if (i < mat.length-1  && mat[i+1][j] == curr0) {
+                        mat[i][j] = Math.min(curr0 + 1, mat[i][j]);
+                        //System.out.printf("Set [%s][%s] = %s\n", i,j, mat[i][j]);
+                        continue;
+                    }
+                    if (j > 0  && mat[i][j-1] == curr0) {
+                        mat[i][j] = Math.min(curr0 + 1, mat[i][j]);
+                        //System.out.printf("Set [%s][%s] = %s\n", i,j, mat[i][j]);
+                        continue;
+                    }
+                    if (j < mat[i].length - 1  && mat[i][j+1] == curr0) {
+                        mat[i][j] = Math.min(curr0 + 1, mat[i][j]);
+                        // System.out.printf("Set [%s][%s] = %s\n", i,j, mat[i][j]);
+                        continue;
+                    }
+                }
+            }
+        }
+        
+        return mat;
+    }
+    
+
+}
