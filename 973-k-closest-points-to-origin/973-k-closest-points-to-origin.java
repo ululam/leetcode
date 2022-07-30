a5723ed652840bb50707ba849fccabb611c36bfa
Alexey Zakharchenko <alexey.zakharchenko@gmail.com>
2022-07-30 14:25:16 +0300

    Time: 45 ms (61.34%), Space: 113.9 MB (77.07%) - LeetHub

--- 973-k-closest-points-to-origin/973-k-closest-points-to-origin.java
+++ 973-k-closest-points-to-origin/973-k-closest-points-to-origin.java
@@ -3,22 +3,12 @@ import java.util.Map.Entry;
 
 class Solution {
     public int[][] kClosest(int[][] points, int k) {
-        final Map<Integer, List<int[]>> dists = new TreeMap<>();
-        for (int i=0; i< points.length; i++) {
-            int dist = points[i][0]*points[i][0] + points[i][1]*points[i][1];
-            List dList = Collections.singletonList(points[i]);
-            dists.merge(dist, dList, (l1, l2) -> 
-                        Stream.concat(l1.stream(), l2.stream())
-                            .collect(Collectors.toList()));
-        }
+        Arrays.sort(points, (a,b) -> dist(a) - dist(b));
         
-        
-        return dists.entrySet().stream()
-            .map(Entry::getValue)
-            .flatMap(List::stream)
-            .limit(k)
-            .collect(Collectors.toList())
-            .toArray(new int[k][2]);
-        
+        return Arrays.copyOf(points, k);
+    }
+    
+    private int dist(int[] p) {
+        return p[0]*p[0] + p[1]*p[1];
     }
 }
