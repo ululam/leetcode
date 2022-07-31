37673b883b27c6c9fcfbb96d80cf2ab43db96bfb
Alexey Zakharchenko <alexey.zakharchenko@gmail.com>
2022-07-31 22:19:06 +0300

    Time: 42 ms (67.47%), Space: 118.2 MB (39.26%) - LeetHub

--- 973-k-closest-points-to-origin/973-k-closest-points-to-origin.java
+++ 973-k-closest-points-to-origin/973-k-closest-points-to-origin.java
@@ -3,12 +3,29 @@ import java.util.Map.Entry;
 
 class Solution {
     public int[][] kClosest(int[][] points, int k) {
-        Arrays.sort(points, (a,b) -> dist(a) - dist(b));
+        final Queue<int[]> pQueue = new PriorityQueue<>((a,b) -> b[0] - a[0]);
+        for (int i=0; i<points.length; i++) {
+            int[] data = new int[] {dist(points[i]), i}; 
+            if (pQueue.size() < k) {
+                pQueue.add(data);
+            } else if (pQueue.peek()[0] > data[0]) {
+                pQueue.poll();
+                pQueue.add(data);
+            }
+        }
         
-        return Arrays.copyOf(points, k);
+        int[][] res = new int[k][2];
+        int i = 0;
+        while (!pQueue.isEmpty()) {
+            int index = pQueue.poll()[1];
+            res[i++] = points[index];
+        }
+            
+        return res;
     }
     
     private int dist(int[] p) {
         return p[0]*p[0] + p[1]*p[1];
     }
+    
 }
