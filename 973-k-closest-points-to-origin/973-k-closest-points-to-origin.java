03f4724abcd3f44b040092dd45ec7d1b4bae8991
Alexey Zakharchenko <alexey.zakharchenko@gmail.com>
2022-08-02 00:31:53 +0300

    Time: 4 ms (99.22%), Space: 113.6 MB (78.84%) - LeetHub

--- 973-k-closest-points-to-origin/973-k-closest-points-to-origin.java
+++ 973-k-closest-points-to-origin/973-k-closest-points-to-origin.java
@@ -3,29 +3,56 @@ import java.util.Map.Entry;
 
 class Solution {
     public int[][] kClosest(int[][] points, int k) {
-        final Queue<int[]> pQueue = new PriorityQueue<>((a,b) -> b[0] - a[0]);
-        for (int i=0; i<points.length; i++) {
-            int[] data = new int[] {dist(points[i]), i}; 
-            if (pQueue.size() < k) {
-                pQueue.add(data);
-            } else if (pQueue.peek()[0] > data[0]) {
-                pQueue.poll();
-                pQueue.add(data);
+        return quickSelect(points, k);
+    }
+    
+    private int[][] quickSelect(int[][] points, int k) {
+        int left = 0;
+        int right = points.length - 1;
+        int pivotIndex = points.length;
+        while (pivotIndex != k) {
+            pivotIndex = partition(points, left, right);
+            if (pivotIndex < k) {
+                left = pivotIndex;
+            } else {
+                right = pivotIndex - 1;
             }
         }
         
-        int[][] res = new int[k][2];
-        int i = 0;
-        while (!pQueue.isEmpty()) {
-            int index = pQueue.poll()[1];
-            res[i++] = points[index];
+        return Arrays.copyOf(points, k);
+    }    
+    
+    private int partition(int[][] points, int left, int right) {
+        final int pivotIndex = selectPivot(left, right);
+        final int[] pivotPoint = points[pivotIndex];
+        final int pivotDist = dist(pivotPoint);
+        
+        while (left < right) {
+            if (dist(points[left]) >= pivotDist) {
+                swap(points, left, right);
+                right--;
+            } else {
+                left++;
+            }
         }
-            
-        return res;
+        
+        // Should break through pivot
+        if (dist(points[left]) < pivotDist) left++;
+        
+        return left;
+    }
+    
+    private void swap(int[][] points, int left, int right) {
+        int[] tmp = points[left];
+        points[left] = points[right];
+        points[right] = tmp;
+    }
+    
+    private int selectPivot(int left, int right) {
+        return (left + right) / 2;
     }
     
     private int dist(int[] p) {
         return p[0]*p[0] + p[1]*p[1];
     }
-    
 }
