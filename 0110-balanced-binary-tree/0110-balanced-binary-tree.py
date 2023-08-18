a308ff84bccd8059f98c61c40c6302fe6c1f3c52
Alexey Zakharchenko <alexey.zakharchenko@gmail.com>
2023-08-18 09:05:49 +0400

    Time: 28 ms (86.29%), Space: 17.8 MB (65.11%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.py
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.py
@@ -5,6 +5,9 @@
 #         self.left = left
 #         self.right = right
 class Solution(object):
+    def __init__(self):
+        self.sizes = {}
+
     def isBalanced(self, root):
         # Tree is balanced if:
         # - abs(len(left) - len(right)) <= 1
@@ -22,4 +25,11 @@ class Solution(object):
     def ln(self, node):
         if not node:
             return 0
-        return max(self.ln(node.left), self.ln(node.right)) + 1
+        
+        size = self.sizes.get(node)
+        if not size:
+            size = max(self.ln(node.left), self.ln(node.right)) + 1
+            self.sizes[node] = size
+
+        return size;
+
