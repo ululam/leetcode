5e23d21b9c901e12a42adcac77f9cb880dd3d6d4
Alexey Zakharchenko <alexey.zakharchenko@gmail.com>
2023-08-22 02:13:42 +0400

    Time: 80 ms (62.46%), Space: 13.9 MB (20.09%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -1,22 +1,13 @@
 class Solution(object):
     def characterReplacement(self, s, k):
-        # ABCBD, 2 - > 2 
-        # ABCCCD, 1 -> 4 (CCCC)
- 
-        # counters = [0] * 26
-        # shift = ord('A') # 65
-        counters = {}
         res = 0
-        maxCharCounter = 0
+        counts = {}
         l = 0
         for r in range(len(s)):
-            #counters[ord(s[r])-shift] += 1 
-            counters[s[r]] =  counters.get(s[r], 0) + 1
-            maxCharCounter = max(maxCharCounter, counters[s[r]])
-            if r - l + 1 - maxCharCounter > k:
-                # counters[ord(s[l])-shift] -= 1
-                counters[s[l]] -= 1
+            counts[s[r]] = 1 + counts.get(s[r], 0)
+            if r - l + 1 - max(counts.values()) > k:
+                counts[s[l]] -= 1
                 l += 1
-            res = r - l + 1
-        return res
 
+            res = max(res, r - l +1)
+        return res
