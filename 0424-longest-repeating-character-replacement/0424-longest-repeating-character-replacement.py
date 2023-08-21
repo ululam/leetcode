069b93da8f019018be4cdf9b1fda1299025e6050
Alexey Zakharchenko <alexey.zakharchenko@gmail.com>
2023-08-22 01:45:27 +0400

    Time: 73 ms (70.61%), Space: 13.9 MB (20.09%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -3,18 +3,18 @@ class Solution(object):
         # ABCBD, 2 - > 2 
         # ABCCCD, 1 -> 4 (CCCC)
  
-        # counters = [0] * 26
-        # shift = ord('A') # 65
-        counters = {}
+        counters = [0] * 26
+        shift = ord('A') # 65
+        # counters = {}
         res = 0
         l = 0
         for r in range(len(s)):
-            #counters[ord(s[r])-shift] += 1 
-            counters[s[r]] = 1 + counters.get(s[r], 0)
+            counters[ord(s[r])-shift] += 1 
+            # counters[s[r]] = 1 + counters.get(s[r], 0)
             windowLen = r - l + 1
-            if windowLen - max(counters.values()) > k:
-                # counters[ord(s[l])-shift] -= 1
-                counters[s[l]] -= 1
+            if windowLen - max(counters) > k:
+                counters[ord(s[l])-shift] -= 1
+                # counters[s[l]] -= 1
                 l += 1
                 continue
             res = max(res, windowLen)
