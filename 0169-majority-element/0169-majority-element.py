3a73aa0791240058ef9eabf0e39418ecb7f792fb
Alexey Zakharchenko <alexey.zakharchenko@gmail.com>
2023-08-25 01:35:51 +0400

    Time: 113 ms (94.87%), Space: 15.4 MB (16.36%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,5 +1,5 @@
 class Solution(object):
-    def majorityElement(self, nums):
+    def majorityElementOnMem(self, nums):
         # [1,2,3,4,4,4,4] => 4
         # [1] => 1
         # [1,2,1] => 1
@@ -12,5 +12,21 @@ class Solution(object):
                 return n
             counts[n] = nCount
         return -1
+
+    def majorityElement(self, nums):
+        candidate = None
+        candidateCount, othersCount = 0,0
+        for i in range(0, len(nums)):
+            if not candidate:
+               candidate = nums[i] 
+            if nums[i] == candidate:
+                candidateCount += 1
+            else:
+                othersCount += 1
+                if candidateCount == othersCount:
+                    candidate = None
+
+
+        return candidate
 
 
