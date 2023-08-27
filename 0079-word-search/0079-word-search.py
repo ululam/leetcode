f1fb4db90cc85976d38c778b0300db766ea97b5d
Alexey Zakharchenko <alexey.zakharchenko@gmail.com>
2023-08-28 02:26:57 +0400

    Time: 4082 ms (84.49%), Space: 16.3 MB (70.85%) - LeetHub

--- 0079-word-search/0079-word-search.py
+++ 0079-word-search/0079-word-search.py
@@ -1,29 +1,19 @@
 class Solution:
-    def __init__(self):
-        self.visited = None
-        self.verified = None
-
-    # 
-
     def exist(self, board: List[List[str]], word: str) -> bool:
         for n in range(len(board)):
             for k in range (len(board[n])):
-                self.visited = set()
                 if self._exist(board, word, n, k):
                     return True
         return False
     
     def _exist(self, board, word, v, h) -> bool:
         # print(f"Checking {word} at ({v},{h})")
-        visitedKey = self._position_key(v, h) 
-        if visitedKey in self.visited:
-            return False
         if board[v][h] != word[0]:
             return False
         if len(word) < 2:
             return True
         
-        self.visited.add(visitedKey)
+        board[v][h] = '#'
         # Go up
         res = False
         if v > 0 and self._exist(board, word[1:], v-1, h):
@@ -38,20 +28,7 @@ class Solution:
         elif h < len(board[v])-1 and self._exist(board, word[1:], v, h+1):
             res = True 
         
-        self.visited.remove(visitedKey)
+        board[v][h] = word[0]
 
         return res
 
-
-    def _remove_cell_and_return(self, x, y, res) -> bool:
-        key = self._position_key(x, y)
-        if key in self.visited:
-            self.visited.remove(key)
-        return res
-
-
-    def _key(self, word, x, y):
-        return f"{word}_{x}_{y}"
-    
-    def _position_key(self, x, y):
-        return self._key("", x, y)
