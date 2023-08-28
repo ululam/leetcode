6e897898c274e5046c0589e923bda163c189fb03
Alexey Zakharchenko <alexey.zakharchenko@gmail.com>
2023-08-28 05:27:20 +0400

    Time: 595 ms (80.07%), Space: 30.4 MB (97.82%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.py
+++ 0053-maximum-subarray/0053-maximum-subarray.py
@@ -2,8 +2,18 @@ class Solution(object):
   def maxSubArray(self, nums: List[int]) -> int:
     maxSum = currentSum = nums[0]
 
-    for i in range(1, len(nums)):
-        currentSum = max(nums[i], currentSum + nums[i])
+    for n in nums[1:]:
+        # if we were sure there are positive numbers in array (at least 1),
+        # we could od (if currentSum < 0) -- drop and start over using the next number.
+        # But that's not true, and we still need to cope with data like [-2,-1,-3]
+        # So we start over when the previous sum is less than the current element
+        if currentSum + n < n:
+            currentSum = n
+        else:
+            currentSum += n
+        # or, we can write:
+        # currentSum = max(nums[i], currentSum + nums[i])
+
         maxSum = max(maxSum, currentSum)
 
     return maxSum
