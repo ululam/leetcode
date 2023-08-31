06865af87ed7d40f84471a7b23f64c70718a57cf
Alexey Zakharchenko <alexey.zakharchenko@gmail.com>
2023-08-31 10:06:41 +0400

    Time: 50 ms (62.67%), Space: 18.8 MB (87.18%) - LeetHub

--- 0328-odd-even-linked-list/0328-odd-even-linked-list.py
+++ 0328-odd-even-linked-list/0328-odd-even-linked-list.py
@@ -10,35 +10,15 @@ class Solution:
 
         odd = head
         even = head.next
+        evenHead = even
 
-        firstEven = even
-
-
-        # 1 -> 2 -> 3 -> 4 -> 5
-        # ^.   ^
-        # a) 1 -> 3 -> 4 -> 5 & 2 -> 3 -> ...
-        #    ^                  ^
-        # b) 1 -> 3 -> 4 -> 5 & 2 -> 3 -> 4 ...
-        #         ^             ^
-        # c) 1 -> 3 -> 4 -> 5 & 2 -> 4 -> 5 -> ...
-        #         ^             ^
-        # d) 1 -> 3 -> 4 -> 5 -> null & 2 -> 4 -> 5 -> null
-        #         ^                  ^
-        # a1) 1 -> 3 -> 4 -> 5 -> null & 2 -> 4 -> 5 -> null
-        #         ^                  ^
-        # b1) 1 -> 3 ->  5 -> null & 2 -> 4 -> 5 -> null
-        #         ^                  ^
-        # c1) 1 -> 3 ->  5 -> null & 2 -> 4 -> 5 -> null
-        #                ^                ^
-        # d1) 1 -> 3 ->  5 -> null & 2 -> 4 ->  null
-        #                ^                ^
         while even and even.next:
-            lastOdd = odd
+            prevOdd = odd
             odd.next = even.next
             odd = odd.next
             even.next = odd.next
             even = even.next
 
-        odd.next = firstEven
+        odd.next = evenHead
 
         return head
