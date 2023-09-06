455b7116337e0df78bd22257a9044d178d6462b6
Alexey Zakharchenko <alexey.zakharchenko@gmail.com>
2023-09-06 09:11:31 +0400

    Time: 542 ms (55.68%), Space: 19.7 MB (52.86%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.py
+++ 0542-01-matrix/0542-01-matrix.py
@@ -36,38 +36,10 @@ class Solution:
         return matrix
 
     def updateMatrixDP(self, mat: List[List[int]]) -> List[List[int]]:
-        dp = [row[:] for row in mat]
-        m, n = len(dp), len(dp[0])
-
-        for row in range(m):
-            for col in range(n):
-                min_neighbor = inf
-                if dp[row][col] != 0:
-                    if row > 0:
-                        min_neighbor = min(min_neighbor, dp[row - 1][col])
-                    if col > 0:
-                        min_neighbor = min(min_neighbor, dp[row][col - 1])
-                        
-                    dp[row][col] = min_neighbor + 1
-    
-        for row in range(m - 1, -1, -1):
-            for col in range(n - 1, -1, -1):
-                min_neighbor = inf
-                if dp[row][col] != 0:
-                    if row < m - 1:
-                        min_neighbor = min(min_neighbor, dp[row + 1][col])
-                    if col < n - 1:
-                        min_neighbor = min(min_neighbor, dp[row][col + 1])
-                        
-                    dp[row][col] = min(dp[row][col], min_neighbor + 1)
-
-        return dp        
-
-    def updateMatrixDP2(self, mat: List[List[int]]) -> List[List[int]]:
         matrix = [row[:] for row in mat]
         m = len(mat)
         n = len(mat[0])
-        maxValue = m*m + 1
+        maxValue = m*n + 1
         # Bottom-Right iteration
         print (">>>> Bottom Right")
         for row in range(m):
