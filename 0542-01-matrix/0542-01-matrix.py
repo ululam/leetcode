d8c86fbf0dcb58947746526d92ad0edc9b6056ad
Alexey Zakharchenko <alexey.zakharchenko@gmail.com>
2023-09-06 09:10:34 +0400

    Time: 435 ms (94.52%), Space: 19.7 MB (52.86%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.py
+++ 0542-01-matrix/0542-01-matrix.py
@@ -2,10 +2,8 @@ from collections import deque
 
 class Solution:
     def updateMatrix(self, mat: List[List[int]]) -> List[List[int]]:
-        return self.updateMatrixBFS(mat)
-
-    def updateMatrixDP(self, mat: List[List[int]]) -> List[List[int]]:
-        pass
+        # return self.updateMatrixBFS(mat)
+        return self.updateMatrixDP(mat)
 
     def updateMatrixBFS(self, mat: List[List[int]]) -> List[List[int]]:
         # The idea is to BFS: if we calculated distances from each 0 simultanuesly, 
@@ -34,5 +32,70 @@ class Solution:
                 if valid(next_row, next_col) and matrix[next_row][next_col] == -1:
                     matrix[next_row][next_col] = distance + 1
                     queue.append((next_row, next_col, distance + 1))
+
+        return matrix
+
+    def updateMatrixDP(self, mat: List[List[int]]) -> List[List[int]]:
+        dp = [row[:] for row in mat]
+        m, n = len(dp), len(dp[0])
+
+        for row in range(m):
+            for col in range(n):
+                min_neighbor = inf
+                if dp[row][col] != 0:
+                    if row > 0:
+                        min_neighbor = min(min_neighbor, dp[row - 1][col])
+                    if col > 0:
+                        min_neighbor = min(min_neighbor, dp[row][col - 1])
+                        
+                    dp[row][col] = min_neighbor + 1
+    
+        for row in range(m - 1, -1, -1):
+            for col in range(n - 1, -1, -1):
+                min_neighbor = inf
+                if dp[row][col] != 0:
+                    if row < m - 1:
+                        min_neighbor = min(min_neighbor, dp[row + 1][col])
+                    if col < n - 1:
+                        min_neighbor = min(min_neighbor, dp[row][col + 1])
+                        
+                    dp[row][col] = min(dp[row][col], min_neighbor + 1)
+
+        return dp        
+
+    def updateMatrixDP2(self, mat: List[List[int]]) -> List[List[int]]:
+        matrix = [row[:] for row in mat]
+        m = len(mat)
+        n = len(mat[0])
+        maxValue = m*m + 1
+        # Bottom-Right iteration
+        print (">>>> Bottom Right")
+        for row in range(m):
+            for col in range(n):
+                if matrix[row][col] == 0:
+                    continue
+                min_neighbour = maxValue
+                if row > 0:
+                    min_neighbour = min(min_neighbour, matrix[row-1][col])
+                if col > 0:
+                    min_neighbour = min(min_neighbour, matrix[row][col-1])
+                matrix[row][col] = 1 + min_neighbour
+                print(f"matrix[{row}][{col}]: {matrix[row][col]}")
+
+        print(f"{matrix}")
+
+        # Top-Left iteration
+        print (">>>> Top Left")
+        for row in range(m-1, -1, -1):
+            for col in range(n-1, -1, -1):
+                if matrix[row][col] == 0:
+                    continue
+                min_neighbour = maxValue
+                if row < m-1:
+                    min_neighbour = min(min_neighbour, matrix[row+1][col])
+                if col < n-1:
+                    min_neighbour = min(min_neighbour, matrix[row][col+1])
+                matrix[row][col] = min(matrix[row][col], 1 + min_neighbour)
+                print(f"matrix[{row}][{col}]: {matrix[row][col]}")
 
         return matrix
