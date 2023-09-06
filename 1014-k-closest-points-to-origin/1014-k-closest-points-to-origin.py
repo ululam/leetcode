797a47da53e9976dbc72938fdbbdadfb78efc7f3
Alexey Zakharchenko <alexey.zakharchenko@gmail.com>
2023-09-06 10:47:06 +0400

    Time: 716 ms (63.89%), Space: 22.4 MB (85.85%) - LeetHub

--- 1014-k-closest-points-to-origin/1014-k-closest-points-to-origin.py
+++ 1014-k-closest-points-to-origin/1014-k-closest-points-to-origin.py
@@ -11,20 +11,5 @@ class Solution:
         #     return points
 
         #return sorted(points, key = lambda p: p[0]**2 + p[1]**2)[:k]
-        heap = []
-        for (x, y) in points:
-            dist = -(x*x + y*y)
-            if len(heap) == k:
-                heapq.heappushpop(heap, (dist, x, y))
-            else:
-                heapq.heappush(heap, (dist, x, y))
-        
-        return [(x,y) for (dist,x, y) in heap]
-        # D = [] #heap for storing tuple (distance, point)
-        # for pt in points:
-        #     x, y = pt[0], pt[1]
-        #     d = x * x + y * y
-        #     heapq.heappush(D, (-d, pt))
-        #     if len(D) > k:
-        #         heapq.heappop(D)
-        # return [pt for (d, pt) in D]        
+
+        return heapq.nsmallest(k, points, lambda p: p[0]**2 + p[1]**2)
