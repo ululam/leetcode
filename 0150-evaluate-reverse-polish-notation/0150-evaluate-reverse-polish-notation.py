601355d28e8317a86847e666de1cd33b4be4dd86
Alexey Zakharchenko <alexey.zakharchenko@gmail.com>
2023-09-07 08:27:22 +0400

    Time: 76 ms (55.51%), Space: 16.8 MB (13.97%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.py
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.py
@@ -6,43 +6,26 @@ class Solution:
             "*": lambda a,b: a * b,
             "/": lambda a,b: a / b
         }
-
+        
     def evalRPN(self, tokens: List[str]) -> int:
-        return self.evalInPlace(tokens)
+        return self.evalViaStack(tokens)
 
     def evalInPlace(self, tokens: List[str]) -> int:
         # Go through list, apply operations in place, replacing (a,b,operations) triple with its result
-        # Tranlate to nodes
-        head = Node()
-        node = head
-        for t in tokens:
-            node.val = t
-            node.next = Node()
-            node.next.prev = node
-            node = node.next
+        pos = 0
+        while len(tokens) > 1:
+            if tokens[pos] not in self.operations.keys():
+                pos += 1
+                continue
+            x = int(tokens[pos-2])
+            y = int(tokens[pos-1])
+            res = self.operations[t](x, y)
+            tokens[pos] = res
+            token.pop(pos - 2)
+            token.pop(pos - 2)
+            pos -=1
 
-        # Last node is odd
-        node.prev.nxt = None
-
-        node = head
-        while node:
-            print(f"{node.val} ->")
-            node = node.next
-
-        node = head
-        while node:
-            if node.val in self.operations.keys():
-                x = int(node.prev.prev.val)
-                y = int(node.prev.val)
-                res = self.operations[node.val](x, y)
-                currentNode = node.prev.prev
-                currentNode.val = res
-                currentNode.next = node.next
-                if node.next:
-                    node.next.prev = node.prev.prev 
-            node = node.next
-
-        return int(head.val)
+        return int(tokens[0])
 
     def evalViaStack(self, tokens: List[str]) -> int:
         # go through list
@@ -59,9 +42,3 @@ class Solution:
             stack.append(self.operations[t](x, y))
         
         return int(stack.pop())
-
-class Node:
-    def __init__(self):
-        self.val = ""
-        self.prev = None
-        self.next = None
