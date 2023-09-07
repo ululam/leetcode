5d9cf222e58eb79c8a78e797de06cb0d50c64d1c
Alexey Zakharchenko <alexey.zakharchenko@gmail.com>
2023-09-07 08:04:41 +0400

    Time: 72 ms (73.48%), Space: 16.8 MB (13.97%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.py
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.py
@@ -1,23 +1,30 @@
 class Solution:
+    def __init__(self):
+        self.operations = {
+            "+": lambda a,b: a + b,
+            "-": lambda a,b: a - b,
+            "*": lambda a,b: a * b,
+            "/": lambda a,b: a / b
+        }
     def evalRPN(self, tokens: List[str]) -> int:
+        return self.evalViaStack(tokens)
+
+    def evalInPlace(self, tokens: List[str]) -> int:
+        # Go through list, apply operations in place, replacing (a,b,operations) triple with its result
+        pass
+
+    def evalViaStack(self, tokens: List[str]) -> int:
         # go through list
         # if number, add to stack
         # if operation, pop two previous numbers from stack, apply opertation, put to stack
         stack = []
-        operations = ["/", "*", "+", "-"]
         for t in tokens:
-            if t not in operations:
+            if t not in self.operations.keys():
                 stack.append(t)
                 continue
+
             y = int(stack.pop())
             x = int(stack.pop())
-            if t == "+":
-                stack.append(x + y)
-            elif t == "-":
-                stack.append(x - y)
-            elif t == "*":
-                stack.append(x * y)
-            elif t == "/":
-                stack.append(x / y)
+            stack.append(self.operations[t](x, y))
         
         return int(stack.pop())
