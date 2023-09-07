ba016a3982bd04cc7d6236188cced11f72c5da4c
Alexey Zakharchenko <alexey.zakharchenko@gmail.com>
2023-09-07 08:24:19 +0400

    Time: 102 ms (7.64%), Space: 20.5 MB (5.56%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.py
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.py
@@ -6,12 +6,44 @@ class Solution:
             "*": lambda a,b: a * b,
             "/": lambda a,b: a / b
         }
+
     def evalRPN(self, tokens: List[str]) -> int:
-        return self.evalViaStack(tokens)
+        return self.evalInPlace(tokens)
 
     def evalInPlace(self, tokens: List[str]) -> int:
         # Go through list, apply operations in place, replacing (a,b,operations) triple with its result
-        pass
+        # Tranlate to nodes
+        head = Node()
+        node = head
+        for t in tokens:
+            node.val = t
+            node.next = Node()
+            node.next.prev = node
+            node = node.next
+
+        # Last node is odd
+        node.prev.nxt = None
+
+        node = head
+        while node:
+            print(f"{node.val} ->")
+            node = node.next
+
+        node = head
+        while node:
+            if node.val in self.operations.keys():
+                x = int(node.prev.prev.val)
+                y = int(node.prev.val)
+                res = self.operations[node.val](x, y)
+                print(f"x,y,t, res = {x}, {y}, {t}, {res}")
+                currentNode = node.prev.prev
+                currentNode.val = res
+                currentNode.next = node.next
+                if node.next:
+                    node.next.prev = node.prev.prev 
+            node = node.next
+
+        return int(head.val)
 
     def evalViaStack(self, tokens: List[str]) -> int:
         # go through list
@@ -28,3 +60,9 @@ class Solution:
             stack.append(self.operations[t](x, y))
         
         return int(stack.pop())
+
+class Node:
+    def __init__(self):
+        self.val = ""
+        self.prev = None
+        self.next = None
