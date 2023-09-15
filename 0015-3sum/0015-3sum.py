99fb873e6957d6e93d83d1afb6ed6c4f3945b68b
Alexey Zakharchenko <alexey.zakharchenko@gmail.com>
2023-09-15 10:00:00 +0400

    Time: 840 ms (88.55%), Space: 20.3 MB (85.79%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,19 +1,26 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        negSumMap = {}
-        res = {}
- 
-        size = len(nums)
-        for i in range(size):
-            negSumMap[-nums[i]] = i
-       
-        for i in range(size-1):
-            for j in range(i+1, size):
-                summa = nums[i] + nums[j]
-                if summa in negSumMap and i != negSumMap[summa] and j != negSumMap[summa]:
-                    v = [nums[i], nums[j], -summa]
-                    key = ",".join([str(n) for n in sorted(v)])
-                    if not key in res:
-                        res[key] = v
+        res = []
+        nums.sort()
 
-        return res.values()
+        for i in range(len(nums)):
+            if nums[i] > 0:
+                break
+            if i == 0 or nums[i-1] != nums[i]:
+                self.twoSum(nums, i, res)
+        return res
+
+    def twoSum(self, nums, i, res):
+        k,K = i+1, len(nums) - 1
+        while k < K:
+            summa = nums[k] + nums[K]
+            if summa < -nums[i]:
+                k += 1
+            elif summa > -nums[i]:
+                K -= 1
+            else:
+                res.append([nums[i], nums[k], nums[K]])
+                k += 1
+                K -= 1
+                while nums[k-1] == nums[k] and k < K:
+                    k += 1
