8704eed47c9ec7d77c43d32883f881ecbf9d7cd3
Alexey Zakharchenko <alexey.zakharchenko@gmail.com>
2023-09-16 10:23:04 +0400

    Time: 46 ms (55.36%), Space: 16.7 MB (88.11%) - LeetHub

--- 0133-clone-graph/0133-clone-graph.py
+++ 0133-clone-graph/0133-clone-graph.py
@@ -7,11 +7,15 @@ class Node:
 """
 
 from typing import Optional
+from collections import deque
 class Solution:
     def __init__(self):
         self.seen = {}
 
     def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
+        return self.cloneGraphBreadthFS(node)
+
+    def cloneGraphDepthFS(self, node: Optional['Node']) -> Optional['Node']:
         if not node:
             return node
 
@@ -24,3 +28,23 @@ class Solution:
         if node.neighbors:
             clone_node.neighbors = [self.cloneGraph(n) for n in node.neighbors];
         return clone_node
+
+    def cloneGraphBreadthFS(self, node: Optional['Node']) -> Optional['Node']:
+        if not node:
+            return node
+
+        queue = deque([node])
+        self.seen[node] = Node(node.val, [])
+
+        while queue:
+            current_node = queue.popleft()
+            for nbor in current_node.neighbors:
+                nbor_clone = self.seen.get(nbor)
+                if not nbor_clone:
+                    nbor_clone = Node(nbor.val, [])                    
+                    self.seen[nbor] = nbor_clone
+                    queue.append(nbor)
+                self.seen[current_node].neighbors.append(nbor_clone)
+        
+        return self.seen[node]
+
