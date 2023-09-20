c43336a558a9393e65e8d8e21c3f7015cc4db3d7
Alexey Zakharchenko <alexey.zakharchenko@gmail.com>
2023-09-20 11:21:23 +0400

    Time: 97 ms (71.67%), Space: 17.6 MB (95.9%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -2,25 +2,25 @@ from collections import deque
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
         indegree = [0] * numCourses
-        adjacents = [[] for _ in range(numCourses)]
+        nbors = [[] for _ in range(numCourses)]
 
-        for pre in prerequisites:
-            adjacents[pre[1]].append(pre[0])
-            indegree[pre[0]] += 1 
-        
+        for pr in prerequisites:
+            indegree[pr[0]] += 1
+            nbors[pr[1]].append(pr[0])
+
         queue = deque()
-        for i in range(numCourses):
-            if indegree[i] == 0:
+        for i, ind in enumerate(indegree):
+            if ind == 0:
                 queue.append(i)
         
-        nodesVisited = 0
+        coursesVisited = 0
         while queue:
-            node = queue.popleft()
-            nodesVisited += 1
+            i = queue.popleft()
+            coursesVisited += 1
+            for nb in nbors[i]:
+                indegree[nb] -= 1
+                if indegree[nb] == 0:
+                    queue.append(nb)
 
-            for nbor in adjacents[node]:
-                indegree[nbor] -= 1
-                if indegree[nbor] == 0:
-                    queue.append(nbor)
-        
-        return nodesVisited == numCourses
+        return coursesVisited == numCourses
+
