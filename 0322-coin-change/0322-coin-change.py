49856fb1123234e9fc8fcc3e3d95a537687ee720
Alexey Zakharchenko <alexey.zakharchenko@gmail.com>
2023-09-24 09:54:34 +0400

    Time: 962 ms (66.34%), Space: 16.7 MB (72.32%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -6,13 +6,13 @@ class Solution:
         return self.coinChangeBottomUp(coins, amount)
 
     def coinChangeBottomUp(self, coins: List[int], amount: int) -> int:
-        dp = [float('inf')] * (amount + 1)
+        dp = [amount + 1] * (amount + 1)
         dp[0] = 0
-        
-        for coin in coins:
-            for x in range(coin, amount + 1):
-                dp[x] = min(dp[x], dp[x - coin] + 1)
-        return dp[amount] if dp[amount] != float('inf') else -1 
+        for am in range (1, amount + 1):
+            for c in coins:
+                if am - c >= 0:
+                    dp[am] = min(dp[am], 1 + dp[am-c])
+        return dp[amount] if dp[amount] <= amount else -1
 
     def coinChangeMem(self, coins: List[int], amount: int) -> int:
         if amount == 0:
