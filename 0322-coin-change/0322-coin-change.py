b38458408f52075ded05cfada92f25aed2135e3b
Alexey Zakharchenko <alexey.zakharchenko@gmail.com>
2023-09-24 09:22:57 +0400

    Time: 1671 ms (7%), Space: 28.1 MB (21.9%) - LeetHub

---
+++ 0322-coin-change/0322-coin-change.py
@@ -0,0 +1,22 @@
+class Solution:
+    def __init__(self):
+        self.cache = {}
+
+
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        if amount == 0:
+            return 0
+        if amount < min(coins):
+            return -1
+        if amount in self.cache:
+            return self.cache[amount]
+        num = 100_000
+        for c in coins:
+            currNum = self.coinChange(coins, amount - c)
+            if currNum != -1 and 1 + currNum < num:
+                num = 1 + currNum
+        num = -1 if num == 100_000 else num
+        self.cache[amount] = num
+        return num
+    
+        
