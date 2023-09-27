13e934c2639521f4ec9be37656e7b8196dc4ea0d
Alexey Zakharchenko <alexey.zakharchenko@gmail.com>
2023-09-27 08:58:00 +0400

    Time: 61 ms (59.61%), Space: 21.3 MB (5.21%) - LeetHub

--- 0155-min-stack/0155-min-stack.py
+++ 0155-min-stack/0155-min-stack.py
@@ -2,51 +2,26 @@ class MinStack:
 
     def __init__(self):
         self.head = None
-        self.min = None
 
     def push(self, val: int) -> None:
-        if not self.min:
-            if not self.head:
-                # If no min, start tracking only if stack was emptied
-                self.min = val
-        else:
-            self.min = min(self.min, val)
         self.head = Node(val, self.head)
-            
-        # print(f"push({val}): {self.head}, min = {self.min}")
 
     def pop(self) -> None:
-        if self.head:
-            res = self.head.val
-            if res == self.min:
-                self.min = None
-            self.head = self.head.next
-            self.min = None
-        else:
-            res = None
-
-        # print(f"pop: {self.head}, min = {self.min}")
-        return res
-
+        val, self.head = (self.head.val, self.head.next) if self.head else (None, None)
+        return val
 
     def top(self) -> int:
-        print(f"top: {self.head}")
         return self.head.val if self.head else None
 
     def getMin(self) -> int:
-        if not self.min and self.head:
-            self.min = float('inf')
-            node = self.head
-            while node:
-                self.min = min(self.min, node.val)
-                node = node.next
-        # print(f"getMin: min = {self.min}")
-        return self.min if self.min < float('inf') else None
+        return self.head.min if self.head else None
         
 class Node:
     def __init__(self, val: int, next = None):
         self.val = val
         self.next = next
+        self.min = min(self.val, next.min) if next else self.val
+
     
     def __str__(self):
         addon = f" -> {self.next}" if self.next else ""
