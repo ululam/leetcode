683d1214a1b427a9a2559ec703b636558d263349
Alexey Zakharchenko <alexey.zakharchenko@gmail.com>
2023-10-23 00:17:50 +0400

    Time: 46 ms (70.7%), Space: 16.7 MB (49.96%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
@@ -3,7 +3,6 @@ class Solution:
         low, high = 0, len(nums) - 1
         while low <= high:
             mid = low + ((high-low)>>1)
-            print(f"\t [{low},{high}] -> {mid} ")
             if nums[mid] == target:
                 return mid
             if nums[0] <= nums[mid]:    # "<=" important to cope with situation when mid = 0
@@ -18,14 +17,4 @@ class Solution:
                     low = mid + 1
                 else:
                     high = mid - 1
-            # if nums[mid] >= nums[0]:
-            #     if target >= nums[low] and target < nums[mid]:
-            #         high = mid-1
-            #     else:
-            #         low = mid + 1
-            # else:
-            #     if target <= nums[-1] and target > nums[mid]:
-            #         low = mid + 1
-            #     else:
-            #         high = mid - 1
         return -1
