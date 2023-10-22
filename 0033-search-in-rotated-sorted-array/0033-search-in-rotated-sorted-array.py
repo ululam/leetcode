7c668d8b712ec6616eeccfaab3e876f8fde14460
Alexey Zakharchenko <alexey.zakharchenko@gmail.com>
2023-10-23 00:17:14 +0400

    Time: 38 ms (96.66%), Space: 16.5 MB (96.28%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
@@ -3,16 +3,29 @@ class Solution:
         low, high = 0, len(nums) - 1
         while low <= high:
             mid = low + ((high-low)>>1)
+            print(f"\t [{low},{high}] -> {mid} ")
             if nums[mid] == target:
                 return mid
-            if nums[mid] >= nums[0]:
-                if target >= nums[low] and target < nums[mid]:
-                    high = mid-1
+            if nums[0] <= nums[mid]:    # "<=" important to cope with situation when mid = 0
+                # Left subarray
+                if target < nums[mid] and target >= nums[0]:
+                    high = mid - 1
                 else:
                     low = mid + 1
             else:
-                if target <= nums[-1] and target > nums[mid]:
+                # Right subarray
+                if target > nums[mid] and target <= nums[-1]:
                     low = mid + 1
                 else:
                     high = mid - 1
+            # if nums[mid] >= nums[0]:
+            #     if target >= nums[low] and target < nums[mid]:
+            #         high = mid-1
+            #     else:
+            #         low = mid + 1
+            # else:
+            #     if target <= nums[-1] and target > nums[mid]:
+            #         low = mid + 1
+            #     else:
+            #         high = mid - 1
         return -1
