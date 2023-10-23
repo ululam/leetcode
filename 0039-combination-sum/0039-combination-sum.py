aba3229bfd12bdbc2721888a6dae3ac0c2fd1d00
Alexey Zakharchenko <alexey.zakharchenko@gmail.com>
2023-10-23 11:35:38 +0400

    Time: 53 ms (86.42%), Space: 16.2 MB (98.4%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.py
+++ 0039-combination-sum/0039-combination-sum.py
@@ -1,5 +1,20 @@
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        #return self.combinationSumDP(candidates, target)
+        return self.combinationSumBackTrack(candidates, target)
+
+    def combinationSumDP(self, candidates: List[int], target: int) -> List[List[int]]:
+        dp = [[] for _ in range(len(candidates) + 1)]
+        for c in candidates:
+            for i in range(c, target+1):
+                for comb in dp[i-c]:
+                    newComb = list(comb)
+                    newComb.append(c)
+                    dp[i].append(newComb)
+        return dp[target]
+
+
+    def combinationSumBackTrack(self, candidates: List[int], target: int) -> List[List[int]]:
         results = []
 
         def backtrack(remain, comb, start):
