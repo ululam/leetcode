b44f5564af90d5096e87ca9029afd83fe423c51f
Alexey Zakharchenko <alexey.zakharchenko@gmail.com>
2023-10-28 08:21:34 +0400

    Time: 57 ms (97.13%), Space: 17.2 MB (15.06%) - LeetHub

--- 1168-duplicate-zeros/1168-duplicate-zeros.py
+++ 1168-duplicate-zeros/1168-duplicate-zeros.py
@@ -1,38 +1,22 @@
-class Solution:
-    def duplicateZeros(self, arr: List[int]) -> None:
-        """
-        Do not return anything, modify arr in-place instead.
-        """
+class Solution(object):
+    def duplicateZeros(self, arr):
+        n = len(arr)-1
+        zeroCount = 0
 
-        possible_dups = 0
-        length_ = len(arr) - 1
-
-        # Find the number of zeros to be duplicated
-        for left in range(length_ + 1):
-
-            # Stop when left points beyond the last element in the original list
-            # which would be part of the modified list
-            if left > length_ - possible_dups:
+        for i in range(n+1):
+            if i > n - zeroCount:
                 break
-
-            # Count the zeros
-            if arr[left] == 0:
-                # Edge case: This zero can't be duplicated. We have no more space,
-                # as left is pointing to the last element which could be included  
-                if left == length_ - possible_dups:
-                    arr[length_] = 0 # For this zero we just copy it without duplication.
-                    length_ -= 1
-                    break
-                possible_dups += 1
-
-        # Start backwards from the last element which would be part of new list.
-        last = length_ - possible_dups
-
-        # Copy zero twice, and non zero once.
-        for i in range(last, -1, -1):
             if arr[i] == 0:
-                arr[i + possible_dups] = 0
-                possible_dups -= 1
-                arr[i + possible_dups] = 0
-            else:
-                arr[i + possible_dups] = arr[i]
+                # Edge case: last 0
+                if i == n - zeroCount:
+                    arr[n] = 0
+                    n -= 1
+                    break
+                zeroCount += 1
+
+        start = n - zeroCount
+        for i in range(start, -1, -1):
+            arr[i + zeroCount] = arr[i]
+            if arr[i] == 0:
+                zeroCount -= 1
+                arr[i + zeroCount] = 0
