ef3fc72b48da36cc84de8cef8ae83feb222225ec
Alexey Zakharchenko <alexey.zakharchenko@gmail.com>
2023-11-03 11:41:00 +0400

    Time: 41 ms (55.7%), Space: 16.2 MB (74.94%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -1,19 +1,16 @@
 class Solution:
     def sortColors(self, nums: List[int]) -> None:
-        # Go through the array, if seen 0, swap with last non-0
-        # If faced 2, do the same from the other end, and continue there
-        # Until both pointers met
-
         low, high = 0, len(nums)-1
         curr = 0
         while curr <= high:
             if nums[curr] == 0:
-                nums[low], nums[curr] = nums[curr], nums[low]
+                nums[curr], nums[low] = nums[low], nums[curr]
                 low += 1
                 curr += 1
             elif nums[curr] == 2:
-                nums[high], nums[curr] = nums[curr], nums[high]
+                nums[curr], nums[high] = nums[high], nums[curr]
                 high -= 1
             else:
                 curr += 1
+
                 
