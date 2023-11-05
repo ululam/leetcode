23e333b9029fc9422f3ab39e556d169754ac4caf
Alexey Zakharchenko <alexey.zakharchenko@gmail.com>
2023-11-06 02:45:22 +0400

    Time: 37 ms (67.21%), Space: 16.3 MB (39.47%) - LeetHub

--- 0807-custom-sort-string/0807-custom-sort-string.py
+++ 0807-custom-sort-string/0807-custom-sort-string.py
@@ -7,9 +7,10 @@ class Solution:
 
         res = []
         for c in order:
-            res += [c] * counts[c]
+            res.append(c * counts[c])
             counts[c] = 0
-        for c in [c for c in counts if counts[c] != 0]:
-            res += [c] * counts[c]
+        for c in counts:
+            # Zeros counted chars are auto-filtered here
+            res.append(c * counts[c])
 
         return "".join(res)
