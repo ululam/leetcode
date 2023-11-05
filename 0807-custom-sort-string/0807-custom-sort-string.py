8d77113866d131cc9536c395d437098bc5f1178f
Alexey Zakharchenko <alexey.zakharchenko@gmail.com>
2023-11-06 02:42:58 +0400

    Time: 41 ms (38.56%), Space: 16.3 MB (8.86%) - LeetHub

---
+++ 0807-custom-sort-string/0807-custom-sort-string.py
@@ -0,0 +1,11 @@
+class Solution:
+    def customSortString(self, order: str, s: str) -> str:
+        counts = collections.Counter(s)
+        res = []
+        for c in order:
+            res += [c] * counts[c]
+            counts[c] = 0
+        for c in [c for c in counts if counts[c] != 0]:
+            res += [c] * counts[c]
+
+        return "".join(res)
