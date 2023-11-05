b3c3eea6d9a123c15e474605772fc5f8112a2477
Alexey Zakharchenko <alexey.zakharchenko@gmail.com>
2023-11-06 02:43:58 +0400

    Time: 39 ms (50.14%), Space: 16.2 MB (74.21%) - LeetHub

--- 0807-custom-sort-string/0807-custom-sort-string.py
+++ 0807-custom-sort-string/0807-custom-sort-string.py
@@ -1,6 +1,10 @@
 class Solution:
     def customSortString(self, order: str, s: str) -> str:
-        counts = collections.Counter(s)
+        #counts = collections.Counter(s)
+        counts = defaultdict(int)
+        for c in s:
+            counts[c] += 1
+
         res = []
         for c in order:
             res += [c] * counts[c]
