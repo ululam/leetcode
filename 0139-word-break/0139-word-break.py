7c3720e5da0a5a4842f7c3ec29f74eb162e42b06
Alexey Zakharchenko <alexey.zakharchenko@gmail.com>
2023-11-05 04:13:18 +0400

    Time: 48 ms (34.51%), Space: 16.5 MB (37.22%) - LeetHub

--- 0139-word-break/0139-word-break.py
+++ 0139-word-break/0139-word-break.py
@@ -1,14 +1,37 @@
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        # return self.wordBreakBFS(s, wordDict)
+        return self.wordBreakDp(s, wordDict)
+
+    def wordBreakDp(self, s, wordDict):
+        memo = {}
+        def dp(i):
+            if i in memo:
+                return memo[i]
+            if i < 0:
+                return True
+            for word in wordDict:
+                start = i - len(word) + 1
+                end = i +1
+                if s[start:end] == word and dp(i - len(word)):
+                    memo[i] = True
+                    return True
+            memo[i] = False
+            return False
+        
+        return dp(len(s)-1)
+
+    # O(n^3 + km), O(n + km)
+    def wordBreakBFS(self, s: str, wordDict: List[str]) -> bool:
         words = set(wordDict)
         queue = deque([0])
         seen = set()
-
+        n = len(s)
         while queue:
             start = queue.popleft()
-            if start == len(s):
+            if start == n:
                 return True
-            for end in range(start+1, len(s)+1):
+            for end in range(start+1, n+1):
                 if end in seen:
                     continue
                 if s[start:end] in words:
