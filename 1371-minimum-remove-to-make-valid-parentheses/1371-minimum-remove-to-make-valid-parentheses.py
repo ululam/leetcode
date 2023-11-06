9077bba515cfb5e63ec3bb23dd998b7b1b4055cd
Alexey Zakharchenko <alexey.zakharchenko@gmail.com>
2023-11-06 04:00:31 +0400

    Time: 99 ms (50.13%), Space: 18.9 MB (13.84%) - LeetHub

--- 1371-minimum-remove-to-make-valid-parentheses/1371-minimum-remove-to-make-valid-parentheses.py
+++ 1371-minimum-remove-to-make-valid-parentheses/1371-minimum-remove-to-make-valid-parentheses.py
@@ -1,5 +1,36 @@
 class Solution:
     def minRemoveToMakeValid(self, s: str) -> str:
+        # return self.minRemoveToMakeValidStack(s)
+        return self.minRemoveToMakeValidStackless(s)
+
+    def minRemoveToMakeValidStackless(self, s: str) -> str:
+        l = list(s)
+        unmatchedOpenCounter, totalOpenCounter = 0, 0
+        for i, c in enumerate(l):
+            if c == ')':
+                if unmatchedOpenCounter == 0:
+                    l[i] = '*'
+                else:
+                    unmatchedOpenCounter -= 1
+            elif c == '(':
+                unmatchedOpenCounter += 1
+                totalOpenCounter += 1
+
+        leaveOpenCount = totalOpenCounter - unmatchedOpenCounter
+        res = []
+        for c in l:
+            if c == '*':
+                continue
+            if c == '(':
+                if leaveOpenCount > 0:
+                    res.append(c)
+                    leaveOpenCount -= 1
+            else:
+                res.append(c)
+        return "".join(res)
+
+
+    def minRemoveToMakeValidStack(self, s: str) -> str:
         stack = []
         for i in range(len(s)):
             c = s[i]
