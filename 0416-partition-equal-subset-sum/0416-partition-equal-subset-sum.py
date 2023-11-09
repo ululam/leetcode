77788462cb4c0c35fa58940c37d1e46d3b9544ee
Alexey Zakharchenko <alexey.zakharchenko@gmail.com>
2023-11-09 10:15:08 +0400

    Time: 372 ms (85.48%), Space: 72.4 MB (21.63%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
@@ -4,12 +4,18 @@ class Solution:
         if summa & 1 == 1: # Odd summa
             return False
         
-        @cache
+        memo = {}
         def dfs(n, subSum) -> bool:
             if subSum == 0:
                 return True
             if n == 0 or subSum < 0:
                 return False
-            return dfs(n-1, subSum - nums[n-1]) or dfs(n-1, subSum)
+            key = f"{n}_{subSum}"
+            if key in memo:
+                return memo[key]
+            
+            res = dfs(n-1, subSum - nums[n-1]) or dfs(n-1, subSum)
+            memo[key] = res
+            return res
 
         return dfs(len(nums) - 1, summa >> 1)
