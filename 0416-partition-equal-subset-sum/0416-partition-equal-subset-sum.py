a9c237b38815233536f0c237262451d2fb50bee6
Alexey Zakharchenko <alexey.zakharchenko@gmail.com>
2023-11-09 10:15:31 +0400

    Time: 269 ms (94.87%), Space: 70.3 MB (23.28%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
@@ -11,12 +11,12 @@ class Solution:
                 return True
             if n == 0 or subSum < 0:
                 return False
-            key = f"{n}_{subSum}"
-            if key in memo:
-                return memo[key]
+            # key = f"{n}_{subSum}"
+            # if key in memo:
+            #     return memo[key]
             
             res = dfs(n-1, subSum - nums[n-1]) or dfs(n-1, subSum)
-            memo[key] = res
+            # memo[key] = res
             return res
 
         return dfs(len(nums) - 1, summa >> 1)
